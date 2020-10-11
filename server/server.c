4049385015e8c73ba70a79e4dd86092d2b93a8d9
manchery <manchery@outlook.com>
2020-10-10 23:24:18 -0700

    server setup

---
+++ server/server.c
@@ -0,0 +1,74 @@
+// Reference: https://en.wikipedia.org/wiki/Berkeley_sockets#Server
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <signal.h>
+#include <pthread.h>
+
+#include "connection.h"
+
+int SocketFD;
+
+void SIGINT_handler(int x)
+{
+    close(SocketFD);
+    exit(EXIT_SUCCESS);
+}
+
+int main(void)
+{
+    struct sockaddr_in sa;
+    SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (SocketFD == -1)
+    {
+        perror("cannot create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&sa, 0, sizeof sa);
+
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(21);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(SocketFD, (struct sockaddr *)&sa, sizeof sa) == -1)
+    {
+        perror("bind failed");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(SocketFD, 10) == -1)
+    {
+        perror("listen failed");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGINT, SIGINT_handler);
+
+    for (;;)
+    {
+        int ConnectFD = accept(SocketFD, NULL, NULL);
+
+        if (0 > ConnectFD)
+        {
+            perror("accept failed");
+            close(SocketFD);
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, connection, (void *)&ConnectFD);
+    }
+
+    close(SocketFD);
+    return EXIT_SUCCESS;
+}
