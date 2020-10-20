38edd50d0d9144a6dc99a18cfa36d63ce5fc1f38
manchery <manchery@outlook.com>
2020-10-19 23:29:18 -0700

    udp pratice done

--- udp/python/client.py
+++ udp/python/client.py
@@ -3,11 +3,12 @@ import socket
 size = 8192
 
 try:
-    msg = input()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(msg, ('localhost', 9876))
-    print(sock.recv(size))
-    sock.close()
+    for i in range(0, 51):
+        msg = str(i)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(msg, ('localhost', 9876))
+        print sock.recv(size)
+        sock.close()
 
 except:
-    print("cannot reach the server")
+    print "cannot reach the server"
