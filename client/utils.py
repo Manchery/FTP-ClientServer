e9f4e50cd25fe445cf11e6a8b4e9f5679541b341
Manchery <manchery@outlook.com>
2020-10-21 12:31:13 +0800

    DNS 80 -> 53

--- client/utils.py
+++ client/utils.py
@@ -10,7 +10,7 @@ import socket
 
 def get_host_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 80))
+    s.connect(('8.8.8.8', 53))
     ip = s.getsockname()[0]
     s.close()
     return ip
